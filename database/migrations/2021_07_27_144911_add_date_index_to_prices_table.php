642d29145528a11071239932954614763d533283
Junx <hugh8845sam@gmail.com>
2021-07-27 23:30:08 +0800

    prices add date index

---
+++ database/migrations/2021_07_27_144911_add_date_index_to_prices_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+class AddDateIndexToPricesTable extends Migration
+{
+    /**
+     * @var string
+     */
+    private $table = 'prices';
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table($this->table, function (Blueprint $table) {
+            $table->index('date');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table($this->table, function (Blueprint $table) {
+            $table->dropIndex('date');
+        });
+    }
+}
