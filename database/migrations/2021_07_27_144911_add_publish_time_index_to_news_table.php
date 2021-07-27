fb87f6c09728ca3d479b362a8028d1c2eee8a3fc
Junx <hugh8845sam@gmail.com>
2021-07-27 22:49:53 +0800

    news add index

---
+++ database/migrations/2021_07_27_144911_add_publish_time_index_to_news_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+class AddPublishTimeIndexToNewsTable extends Migration
+{
+    /**
+     * @var string
+     */
+    private $table = 'news';
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table($this->table, function (Blueprint $table) {
+            $table->index('publish_time');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table($this->table, function (Blueprint $table) {
+            $table->dropIndex('publish_time');
+        });
+    }
+}
