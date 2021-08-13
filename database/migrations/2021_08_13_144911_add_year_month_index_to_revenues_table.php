11282ef153753eea6af2500e9c6c9b694a6a5974
Junx <hugh8845sam@gmail.com>
2021-08-13 18:55:21 +0800

    新增index

---
+++ database/migrations/2021_08_13_144911_add_year_month_index_to_revenues_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+class AddYearMonthIndexToRevenuesTable extends Migration
+{
+    /**
+     * @var string
+     */
+    private $table = 'revenues';
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table($this->table, function (Blueprint $table) {
+            $table->unique(['stock_id', 'year', 'month']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table($this->table, function (Blueprint $table) {
+            $table->dropColumn('yoy');
+            $table->dropColumn('qoq');
+        });
+    }
+}
