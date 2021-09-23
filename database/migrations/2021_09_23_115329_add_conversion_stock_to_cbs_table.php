7a5183d45e42e2c90bba7b499c1b34c9e33ede98
Junx <hugh8845sam@gmail.com>
2021-09-23 19:32:59 +0800

    可轉債table新增轉換股數

---
+++ database/migrations/2021_09_23_115329_add_conversion_stock_to_cbs_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+class AddConversionStockToCbsTable extends Migration
+{
+    /**
+     * @var string
+     */
+    private $table = 'cbs';
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table($this->table, function (Blueprint $table) {
+            $table->smallInteger('conversion_stock')->after('conversion_price')->comment('轉換股數');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table($this->table, function (Blueprint $table) {
+            $table->dropColumn('conversion_stock');
+        });
+    }
+}
