c9112d693a3cc26c49cb33913e607801430336bd
Junx <hugh8845sam@gmail.com>
2021-10-22 22:49:59 +0800

    rename

--- database/migrations/2021_10_22_165329_add_code_to_funds_table.php
+++ database/migrations/2021_10_22_165329_add_code_to_funds_table.php
@@ -5,7 +5,7 @@ use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
-class AddRationToProfitsTable extends Migration
+class AddCodeToFundsTable extends Migration
 {
     /**
      * @var string
