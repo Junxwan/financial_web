35c63690a1a95b90a70e9216a97f880a2db38979
Junx <hugh8845sam@gmail.com>
2021-10-22 22:45:45 +0800

    基金增加統編欄位

---
+++ database/migrations/2021_10_22_165329_add_code_to_funds_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+class AddRationToProfitsTable extends Migration
+{
+    /**
+     * @var string
+     */
+    private $table = 'funds';
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table($this->table, function (Blueprint $table) {
+            $table->string('code', 20)->after('company_id')->comment('統編');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table($this->table, function (Blueprint $table) {
+            $table->dropColumn('code');
+        });
+    }
+}
